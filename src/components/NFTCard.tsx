4954ed1dc2d5e4edb354f3ee17e9d326c5a839a3
Danial Aiman Jamil <58195654+KawakazeRin@users.noreply.github.com>
2023-02-02 15:55:24 +0800

    fix

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -1,11 +1,11 @@
 export default function NFTCard() {
 	return (
-		<div className='bg-card w-[clamp(250px,_50%,_300px)] aspect-square box-content p-4 rounded-xl text-[18px] hover:border-2 border-line'>
-			<img
-				className='rounded-xl aspect-square object-contain'
-				src='./image-equilibrium.jpg'
-				alt='Equilibrium #3429'
-			/>
+		<div className='bg-card w-[clamp(250px,_50%,_300px)] aspect-square box-content p-4 rounded-xl text-[18px]'>
+			<div className='w-full rounded-xl overflow-clip aspect-square object-contain [background-image:_url("./image-equilibrium.jpg")] bg-contain'>
+				<div className='opacity-0 hover:opacity-100 flex justify-center items-center w-full h-full hover:bg-gradient-to-b from-transparent to-black'>
+					<object data='./icon-view.svg' type='image/svg+xml' />
+				</div>
+			</div>
 			<h1 className='font-semibold my-4'>Equilibrium #3429</h1>
 			<p className='font-light my-2'>
 				Our Equilibrium collection promotes balance and calm.
