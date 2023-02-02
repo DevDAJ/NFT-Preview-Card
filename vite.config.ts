7c2a2d7b4761f1116d4df7886dd8810003eb211d
Danial Aiman Jamil <58195654+KawakazeRin@users.noreply.github.com>
2023-02-02 14:04:39 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: '/frontendmentor.io-nft-preview-card/',
+	base: '/NFT-Preview-Card/',
 });
