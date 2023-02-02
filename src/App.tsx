1c4ba49e9cdecb3bfdbcc4b81d56d4cb9f41d302
Danial Aiman Jamil <58195654+KawakazeRin@users.noreply.github.com>
2023-02-02 12:57:35 +0800

    !: Created

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer';
+import NFTCard from './components/NFTCard';
+
+function App() {
+	return (
+		<>
+			<main className='flex justify-center items-center m-auto'>
+				<NFTCard />
+			</main>
+			<Footer />
+		</>
+	);
+}
+
+export default App;
