0dab2d450ab1312ac7a33fcfa95313d0f83e71b8
Danial Aiman Jamil <58195654+KawakazeRin@users.noreply.github.com>
2023-02-02 13:47:47 +0800

    image fix

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -3,7 +3,7 @@ export default function NFTCard() {
 		<div className='bg-card w-[clamp(250px,_50%,_300px)] aspect-square box-content p-4 rounded-xl text-[18px] hover:border-2 border-line'>
 			<img
 				className='rounded-xl aspect-square object-contain'
-				src='/image-equilibrium.jpg'
+				src='./image-equilibrium.jpg'
 				alt='Equilibrium #3429'
 			/>
 			<h2 className='font-semibold my-4'>Equilibrium #3429</h2>
@@ -11,15 +11,15 @@ export default function NFTCard() {
 				Our Equilibrium collection promotes balance and calm.
 			</p>
 			<div className='my-4 flex justify-between items-center flex-wrap text-[1rem] leading-[1rem]'>
-				<IconInfo color='text-cyan-300' icon='/icon-ethereum.svg    '>
+				<IconInfo color='text-cyan-300' icon='./icon-ethereum.svg    '>
 					0.041 ETH
 				</IconInfo>
-				<IconInfo icon='/icon-clock.svg'>3 days left</IconInfo>
+				<IconInfo icon='./icon-clock.svg'>3 days left</IconInfo>
 			</div>
 			<div className='after:my-4 after:content-[""] after:block after:h-[1px] after:bg-line'></div>
 			<div className='flex items-center text-sm gap-1'>
 				<img
-					src='/image-avatar.png'
+					src='./image-avatar.png'
 					className='h-7 border rounded-full mr- 2'
 				/>
 				<span>Creation of</span>
